dbbb70f1170d0eb7afda337941cc6bed12843d43
EndlessRevolution <blank9.page@gmail.com>
2026-04-01 06:17:40 +0400

    refactor: transferring size macros to header file

--- kernel/vmmc.h
+++ kernel/vmmc.h
@@ -6,9 +6,13 @@
 struct vmmc_cmd {
     unsigned int cur_block;
     unsigned int block_num;
-    void __user *data;
+    void *data;
 };
 
+#define ONE_BLOCK_SIZE 512
+#define MAX_BLOCKS 2048
+#define VMMC_MEMORY (ONE_BLOCK_SIZE * MAX_BLOCKS)
+#define DEVICE_NAME "virtual_mmc_driver"
 #define VMMC_MAGIC '#'
 #define VMMC_READ_SINGLE_BLOCK    _IOR(VMMC_MAGIC, 1, struct vmmc_cmd)
 #define VMMC_READ_MULTIPLE_BLOCK  _IOR(VMMC_MAGIC, 2, struct vmmc_cmd)
